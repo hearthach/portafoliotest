fb03a0c0d04e60f9c511fdda83932ecefebecc10
Pedro Jesus Landauro Quiroz <plandauro@enotria.pe>
2023-04-10 16:48:38 -0500

    subida desde trabajo

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Portafolio = () => {
   return (
-    <div>
-      <h1>Portafolio</h1>
+    <div className='page'>
+      <h1 className='heading'>Portafolio</h1>
       </div>
   )
 }
