ca14e07ec045b44bd074b2e36c8baf42673421bc
Pedro Jesus Landauro Quiroz <plandauro@enotria.pe>
2023-04-11 17:31:04 -0500

    modificando portafolio

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { trabajos } from '../data/Trabajos';
 
 export const Portafolio = () => {
   return (
     <div className='page'>
       <h1 className='heading'>Portafolio</h1>
+
+        {
+          trabajos.map(trabajo => {
+            <article>
+              <h2>{trabajo.nombre}</h2>
+            </article>
+          })
+        }
+
       </div>
   )
 }
---
+++ src/data/trabajos.js
@@ -0,0 +1,37 @@
+export const trabajos = [
+    {
+        'id': 'unabweb',
+        'nombre': 'Universidad Nacional de Barranca',
+        'url' : 'https://www.unab.edu.pe/',
+        'tecnologias' : 'HTML, CSS, JS, PHP, MySQL',
+        'categorias' : 'Desarrollo web'
+    },
+    {
+        'id': 'portalweb',
+        'nombre': 'Portal web Informativo',
+        'url' : 'https://moralesmunoz.com/',
+        'tecnologias' : 'HTML, CSS, JS, REACT',
+        'categorias' : 'CMS web'
+    },
+    {
+        'id': 'linkedinPerfil',
+        'nombre': 'LinkedIn',
+        'url' : 'https://www.linkedin.com/',
+        'tecnologias' : 'React, CSS, JS, MongoDB',
+        'categorias' : 'Desarrollo web'
+    },
+    {
+        'id': 'udemy',
+        'nombre': 'Udemy',
+        'url' : 'https://www.udemy.com/',
+        'tecnologias' : 'NodeJS, CSS, JS, VueJS, MySQL',
+        'categorias' : 'Desarrollo web'
+    },
+    {
+        'id': 'sigopsp',
+        'nombre': 'Udemy',
+        'url' : 'http://operacionespspperu.com/',
+        'tecnologias' : 'NetCore, CSS, JS, HTML, SQL',
+        'categorias' : 'Sistemas web'
+    }
+];
